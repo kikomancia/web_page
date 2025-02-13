bd551f198ca7b26b5836a96d590b3b7fcf0f3edc
kikomancia <jerichopmancia@gmail.com>
2025-02-13 11:58:32 +0800

    fixing text sizes

---
+++ resources/motors.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        document.getElementById("loading-screen").classList.add("hidden");
+    }, 500); // Adjust timing as needed
+});
